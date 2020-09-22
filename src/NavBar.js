a43c847aa9e058c1e32b81d321e67c0bc4b8ecd0
AATataj <muresan44@gmail.com>
2020-09-22 17:31:50 -0400

    created the nav bar for the dashboard

--- src/NavBar.js
+++ src/NavBar.js
@@ -9,12 +9,55 @@ import MenuItem from '@material-ui/core/MenuItem';
 
 export default function NbaBar(){
     const [anchorEl, setAnchorEl] = React.useState(null);
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
+    const [anchorEli, setAnchorEli] = React.useState(null);
+    const handleClick= (event) => {
+        const eventID = event.currentTarget.id;
+        console.log(eventID);
+        if (eventID === "menuScraper"){
+            setAnchorEl(event.currentTarget);
+        }
+        else if (eventID === "dbQuery"){
+            console.log("pull up db query component");
+        }
+        else if (eventID === "addFeature"){
+            console.log("pull up add feature component");
+        }
+        else if (eventID === "scrapeAll"){
+            console.log("pull up scraping page with process of all scrapers");
+            handleClose();
+        }
+        else if (eventID === "scrapeRoto"){
+            console.log("pull up scraping page with process of rotoworld ");
+            handleClose();
+        }
+        else if (eventID === "scrapeBox"){
+            console.log("pull up scraping page with process of boxscores");
+            handleClose();
+        }
+        else if (eventID === "scrapePlay"){
+            console.log("pull up scraping page with process of play-by-plays");
+            handleClose();
+        }
+        else if (eventID === "menuModels"){
+            setAnchorEli(event.currentTarget);
+        }
+        else if (eventID === "linReg"){
+            console.log("bring up linear regression testing page");
+            handleClose();
+        }
+        else if (eventID === "sentiment"){
+            console.log("bring up sentiment analysis testing page");
+            handleClose();
+        }
+        else if (eventID === "reinforcement"){
+            console.log("bring up reinforcement learning testing page");
+            handleClose();
+        } 
     };    
     const handleClose = () => {
         setAnchorEl(null);
-    };
+        setAnchorEli(null);
+    }; 
     return(
     <React.Fragment>
     <AppBar position="fixed">
@@ -22,23 +65,61 @@ export default function NbaBar(){
             <IconButton 
                 edge="start" 
                 onClick = {handleClick}
-                id = "menuButton"
-                aria-controls="functionsMenu"
+                id = "menuScraper"
+                aria-controls="scrapersMenu"
                 aria-haspopup="true" 
             >
-                <MenuIcon/>   
+                <MenuIcon/>  
+                Data Scraper 
+            </IconButton>
+            <IconButton
+                edge="start" 
+                onClick = {handleClick}
+                id = "menuModels"
+                aria-controls="functionsMenu"
+                aria-haspopup="true"
+            >
+                <MenuIcon/>Test Models
+            </IconButton>
+            <IconButton
+                edge="start"
+                onClick = {handleClick}
+                id = "dbQuery"
+                
+            >
+                Database Query
+            </IconButton>
+            <IconButton
+                edge="start"
+                onClick = {handleClick}
+                id = "addFeature"
+            >
+                Add Feature
             </IconButton>
             <Menu 
-                id = "functionsMenu"
+                id = "modelsMenu"
+                keepMounted
+                anchorEl={anchorEli}
+                open={Boolean(anchorEli)}
+                onClose={handleClose}        
+            >
+                <MenuItem id="linReg" onClick={handleClick}>Statistical Output</MenuItem>
+                <MenuItem id="sentiment" onClick={handleClick}>Availability</MenuItem>
+                <MenuItem id="reinforcement" onClick={handleClick}>AI GM</MenuItem>
+            </Menu>
+            <Menu 
+                id = "scrapersMenu"
                 keepMounted
                 anchorEl={anchorEl}
                 open={Boolean(anchorEl)}
                 onClose={handleClose}        
             >
-                <MenuItem onClick={handleClose}>item1</MenuItem>
-                <MenuItem onClick={handleClose}>item2</MenuItem>
+                <MenuItem id="scrapeAll" onClick={handleClick}>Scrape All</MenuItem>
+                <MenuItem id="scrapeRoto" onClick={handleClick}>Rotoworld</MenuItem>
+                <MenuItem id="scrapeBox" onClick={handleClick}>Boxscores</MenuItem>
+                <MenuItem id="scrapePlay" onClick={handleClick}>Play-by-Play</MenuItem>
             </Menu>
-            {"The top menu where I'll put links to each feature in the dashboard"}
+            
         </Toolbar>
     </AppBar>
     <Toolbar />
