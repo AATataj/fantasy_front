1d98300c77c49b5246f45902b36a82aea014e6c5
AATataj <muresan44@gmail.com>
2020-09-29 14:56:23 -0400

    adding a stately menu to the dbQuery page

--- src/DBquery.js
+++ src/DBquery.js
@@ -1,18 +1,90 @@
 import React from 'react';
 import './App.css';
 import './NavBar.js';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: 'black', //theme.palette.background.paper,
+    color: 'white'
+  },
+}));
+
+const options = [
+  'Show some love to Material-UI',
+  'Show all notification content',
+  'Hide sensitive notification content',
+  'Hide all notification content',
+];
 
 function DBquery() {
+
+ const classes = useStyles();
+ const [anchorEl, setAnchorEl] = React.useState(null);
+ const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+ const handleClickListItem = (event) => {
+   setAnchorEl(event.currentTarget);
+ };
+
+ const handleMenuItemClick = (event, index) => {
+   setSelectedIndex(index);
+   setAnchorEl(null);
+ };
+
+ const handleClose = () => {
+   setAnchorEl(null);
+ };
   return (
-    <div className="App">
+    <div className="DBQuery">
       <header className="App-header">
         <p>
           Welcome to the NBA Fantasy AI Dashboard <br />
           This is the DB query page
         </p>
       </header>
+      <React.Fragment>
+        <div className={classes.root}>
+        <List component="nav" aria-label="Device settings">
+          <ListItem
+            button
+            aria-haspopup="true"
+            aria-controls="lock-menu"
+            aria-label="when device is locked"
+            onClick={handleClickListItem}
+          >
+            <ListItemText primary="When device is locked" secondary={options[selectedIndex]} />
+          </ListItem>
+        </List>
+        <Menu
+          id="lock-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          {options.map((option, index) => (
+            <MenuItem
+              key={option}
+              disabled={index === 0}
+              selected={index === selectedIndex}
+              onClick={(event) => handleMenuItemClick(event, index)}
+            >
+              {option}
+            </MenuItem>
+          ))}
+        </Menu>
+      </div>
+    </React.Fragment>
     </div>
   );
 }
 
 export default DBquery;
+
+
--- src/NavBar.js
+++ src/NavBar.js
@@ -4,94 +4,110 @@ import Toolbar from '@material-ui/core/Toolbar';
 import '@material-ui/core/MenuItem';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import HomeIcon from '@material-ui/icons/Home'; 
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      backgroundColor: 'black', //theme.palette.background.paper,
+      color: 'white'
+    },
+}));
 
 export default function NbaBar(props){ 
+    const classes = useStyles();
     return(
     <React.Fragment>
-    <AppBar position="fixed">
-        <Toolbar>
-            <IconButton 
-                edge="start" 
-                onClick = {props.handleClick}
-                id = "menuScraper"
-                aria-controls="scrapersMenu"
-                aria-haspopup="true" 
-            >
-                <MenuIcon/>  
-                Data Scraper 
-            </IconButton>
-            <IconButton
-                edge="start" 
-                onClick = {props.handleClick}
-                id = "menuModels"
-                aria-controls="functionsMenu"
-                aria-haspopup="true"
-            >
-                <MenuIcon/>Test Models
-            </IconButton>
-        <Link to = "/DBQuery" style={{ textDecoration: 'none' }}>
-            <IconButton
-                edge="start"
-                onClick = {props.handleClick}
-                id = "dbQuery"
+    <div className={classes.root}>
+        <AppBar position="fixed">
+            <Toolbar>
+                <IconButton 
+                    edge="start" 
+                    onClick = {props.handleClick}
+                    id = "menuScraper"
+                    aria-controls="scrapersMenu"
+                    aria-haspopup="true" 
+                >
+                    <MenuIcon/>  
+                    Data Scraper 
+                </IconButton>
+                <IconButton
+                    edge="start" 
+                    onClick = {props.handleClick}
+                    id = "menuModels"
+                    aria-controls="functionsMenu"
+                    aria-haspopup="true"
+                >
+                    <MenuIcon/>Test Models
+                </IconButton>
+            <Link to = "/DBQuery" style={{ textDecoration: 'none' }}>
+                <IconButton
+                    edge="start"
+                    onClick = {props.handleClick}
+                    id = "dbQuery"
+                    
+                >
+                    Database Query
+                </IconButton>
+            </Link>
+            <Link to = "/AddFeature" style={{ textDecoration: 'none' }}>
+                <IconButton
+                    edge="start"
+                    onClick = {props.handleClick}
+                    id = "addFeature"
+                >
+                    Add Feature
+                </IconButton>
+            </Link>
+            <Link to = "/Welcome" style={{ textDecoration: 'none' }}>
+                <HomeIcon></HomeIcon>
+            </Link>
+                <Menu 
+                    id = "modelsMenu"
+                    keepMounted
+                    anchorEl={props.ModelsAnchor}
+                    open={Boolean(props.ModelsAnchor)}
+                    onClose={props.handleClose}        
+                >
+                    <Link to = "/InProgress" style={{ textDecoration: 'none' }}>
+                        <MenuItem id="linReg" onClick={props.handleClick}>Statistical Output</MenuItem>
+                    </Link>
+                    <Link to = "/InProgress" style={{ textDecoration: 'none' }}>
+                        <MenuItem id="sentiment" onClick={props.handleClick}>Availability</MenuItem>
+                    </Link>
+                    <Link to = "/InProgress" style={{ textDecoration: 'none' }}>
+                        <MenuItem id="reinforcement" onClick={props.handleClick}>AI GM</MenuItem>
+                    </Link>
+                </Menu>
+                <Menu 
+                    id = "scrapersMenu"
+                    keepMounted
+                    anchorEl={props.ScraperAnchor}
+                    open={Boolean(props.ScraperAnchor)} 
+                    onClose={props.handleClose}        
+                >
+                    <Link to = "/InProgress" style={{ textDecoration: 'none' }}>
+                        <MenuItem id="scrapeAll" onClick={props.handleClick}>Scrape All</MenuItem>
+                    </Link>
+                    <Link to = "/InProgress" style={{ textDecoration: 'none' }}>
+                        <MenuItem id="scrapeRoto" onClick={props.handleClick}>Rotoworld</MenuItem>
+                    </Link>
+                    <Link to = "/InProgress" style={{ textDecoration: 'none' }}>
+                        <MenuItem id="scrapeBox" onClick={props.handleClick}>Boxscores</MenuItem>
+                    </Link>
+                    <Link to = "/InProgress" style={{ textDecoration: 'none' }}>
+                        <MenuItem id="scrapePlay" onClick={props.handleClick}>Play-by-Play</MenuItem>
+                    </Link>
+
+                </Menu>
                 
-            >
-                Database Query
-            </IconButton>
-        </Link>
-        <Link to = "/AddFeature" style={{ textDecoration: 'none' }}>
-            <IconButton
-                edge="start"
-                onClick = {props.handleClick}
-                id = "addFeature"
-            >
-                Add Feature
-            </IconButton>
-        </Link>
-            <Menu 
-                id = "modelsMenu"
-                keepMounted
-                anchorEl={props.ModelsAnchor}
-                open={Boolean(props.ModelsAnchor)}
-                onClose={props.handleClose}        
-            >
-                <Link to = "/InProgress" style={{ textDecoration: 'none' }}>
-                    <MenuItem id="linReg" onClick={props.handleClick}>Statistical Output</MenuItem>
-                </Link>
-                <Link to = "/InProgress" style={{ textDecoration: 'none' }}>
-                    <MenuItem id="sentiment" onClick={props.handleClick}>Availability</MenuItem>
-                </Link>
-                <Link to = "/InProgress" style={{ textDecoration: 'none' }}>
-                    <MenuItem id="reinforcement" onClick={props.handleClick}>AI GM</MenuItem>
-                </Link>
-            </Menu>
-            <Menu 
-                id = "scrapersMenu"
-                keepMounted
-                anchorEl={props.ScraperAnchor}
-                open={Boolean(props.ScraperAnchor)} 
-                onClose={props.handleClose}        
-            >
-                <Link to = "/InProgress" style={{ textDecoration: 'none' }}>
-                    <MenuItem id="scrapeAll" onClick={props.handleClick}>Scrape All</MenuItem>
-                </Link>
-                <Link to = "/InProgress" style={{ textDecoration: 'none' }}>
-                    <MenuItem id="scrapeRoto" onClick={props.handleClick}>Rotoworld</MenuItem>
-                </Link>
-                <Link to = "/InProgress" style={{ textDecoration: 'none' }}>
-                    <MenuItem id="scrapeBox" onClick={props.handleClick}>Boxscores</MenuItem>
-                </Link>
-                <Link to = "/InProgress" style={{ textDecoration: 'none' }}>
-                    <MenuItem id="scrapePlay" onClick={props.handleClick}>Play-by-Play</MenuItem>
-                </Link>
-            </Menu>
-            
-        </Toolbar>
-    </AppBar>
-    <Toolbar />
+            </Toolbar>
+        </AppBar>
+        <Toolbar />
+    </div>
     </React.Fragment>
     );
 }
