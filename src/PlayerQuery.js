8fafabc3e8c1b75d5d074ad5062f5a4867bf37a1
AATataj <muresan44@gmail.com>
2020-09-30 15:09:01 -0400

    created json object for 1x request

--- src/PlayerQuery.js
+++ src/PlayerQuery.js
@@ -2,15 +2,23 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 
 
 function PlayerQuery(){
     const submit = (event) => {
         // add http get request stuff here.
+        var requestData = {};
+        requestData.name = document.getElementById('playerName').value;
+        requestData.startDate = document.getElementById('startDate').value;
+        requestData.endDate = document.getElementById('endDate').value;
+        requestData.playerID = parseInt(document.getElementById('playerID').value);
+        console.log(JSON.stringify(requestData));
+
+
     };
     return ( 
         <React.Fragment>
+            <form>
                 <br />
                 Individual Player Stats
                 <br/><br/><br/>
@@ -39,12 +47,13 @@ function PlayerQuery(){
                 <Grid item xs={3} />
                 <Grid item xs={3}>
                 <div style={{ position: 'left' }}>
-                    <Button variant='contained'  onClick={submit}>
+                    <Button variant='contained'  onClick={(event) => submit(event)}>
                         SUBMIT
                     </Button>
                 </div>
                 </Grid>
             </Grid>
+            </form>
         </React.Fragment>
     ); 
 } 
