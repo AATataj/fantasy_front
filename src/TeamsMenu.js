382b4baa0b4a001254a07b6d2c0361f179781e73
AATataj <muresan44@gmail.com>
2020-10-14 14:42:08 -0400

    working drop downs.  OFC antd worked better than material ui

--- src/TeamsMenu.js
+++ src/TeamsMenu.js
@@ -4,9 +4,10 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Collapse from '@material-ui/core/Collapse';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-
+// import Menu from '@material-ui/core/Menu';
+// import MenuItem from '@material-ui/core/MenuItem';
+import { Menu, Dropdown } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
 
 const options = [   
     { 
@@ -35,189 +36,73 @@ const options = [
     }  
 ]            
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
-  },
-  nested: {
-    paddingLeft: theme.spacing(4),
-  },
-}));
-
 export default function TeamsMenu() {
-  const classes = useStyles();
-  const [openAtl, setOpenAtl] = React.useState(false);
-  const [openCen, setOpenCen] = React.useState(false);
-  const [openSE, setOpenSE] = React.useState(false);
-  const [openNW, setOpenNW] = React.useState(false);
-  const [openPac, setOpenPac] = React.useState(false);
-  const [openSW, setOpenSW] = React.useState(false);
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [selectedIndex, setSelectedIndex] = React.useState(null);
+  const SubMenu = Menu.SubMenu;
+  const [selectedTeam, setSelectedTeam] = React.useState(null);
 
-  const handleClickListItem = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleDivClick = (event) => {
-    //setOpen(!open);
-    const tgt = event.currentTarget.id;
-    if (tgt === "Atlantic")
-        setOpenAtl(!openAtl);
-    else if (tgt === "Central")
-        setOpenCen(!openCen);
-    else if (tgt === "Southeast")
-        setOpenSE(!openSE);
-    else if (tgt === "Northwest")
-        setOpenNW(!openNW);
-    else if (tgt === "Pacific")
-        setOpenPac(!openPac);
-    else if (tgt === "Southwest")
-        setOpenSW(!openSW);
-    console.log(tgt);
-  };
-  const handleDivItemClick = (event, index) => {
-    setSelectedIndex(index);
-    console.log(options[index].division);
-    console.log(options[selectedIndex+1].teams[0]);
-    setAnchorEl(null);
-    if (options[index].division === "Atlantic")
-        setOpenAtl(!openAtl);
-    else if (options[index].division === "Central")
-        setOpenCen(!openCen);
-    else if (options[index].division === "Southeast")
-        setOpenSE(!openSE);
-    else if (options[index].division === "Northwest")
-        setOpenNW(!openNW);
-    else if (options[index].division === "Pacific")
-        setOpenPac(!openPac);
-    else if (options[index].division === "Southwest")
-        setOpenSW(!openSW);
-  };
+  const handleClick = (event) => {
+    console.log(event.key);
+    setSelectedTeam(event.key);
 
-  const handleClose = () => {
-    //do something
-  };
-
-  return (
-      <React.Fragment>
-    <List
-      component="nav"
-      aria-labelledby="nested-list-subheader"
-      className={classes.root}
-    >
-      <ListItem 
-            button 
-            onClick={handleClickListItem} 
-            id='leagueList'
-            >
-          <ListItemText primary={
-              !openAtl && !openCen && !openSE && !openNW && !openPac && !openSW 
-              ? "Select Team" 
-              : options[selectedIndex].division} />
-      </ListItem>
-      <Collapse in={openAtl} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding id={selectedIndex===null? "" : options[selectedIndex].division}>
-          <ListItem button className={classes.nested}>
-            {/* <ListItemText primary="Starred" /> */}
-            {options.map((option, index) => (
-            <ListItemText
-              // key={options[selectedIndex].teams[index]}
-              //selected={index === selectedIndex}
-              onClick={(event) => handleDivItemClick(event, index)}
-            >
-              {option.teams[index]}
-            </ListItemText>
-          ))}
-          </ListItem>
-        </List>
-      </Collapse>
-      
-    </List>
+  }
+  const menu = (
     <Menu
-        id="tmMenu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-    >
-        {options.map((option, index) => (
-            <MenuItem
-              key={option.division}
-              selected={index === selectedIndex}
-              onClick={(event) => handleDivItemClick(event, index)}
-            >
-              {option.division}
-            </MenuItem>
-          ))}
-
-    </Menu>
-    </React.Fragment>   
+    // onClick={handleClick}
+    style={{ width: 240 }}
+    mode="vertical"
+  >
+    <SubMenu key="sub1" 
+    title="Atlantic">
+        <Menu.Item key="TOR"  onClick={handleClick}>TOR</Menu.Item>
+        <Menu.Item key="BOS"  onClick={handleClick}>BOS</Menu.Item>
+        <Menu.Item key="PHI" onClick={handleClick}>PHI</Menu.Item>
+        <Menu.Item key="BKY" onClick={handleClick}>BKY</Menu.Item>
+        <Menu.Item key="NYK" onClick={handleClick}>NYK</Menu.Item>
+    </SubMenu>
+    <SubMenu key="sub2" title="Central">
+      <Menu.Item key="MIL" onClick={handleClick}>MIL</Menu.Item>
+      <Menu.Item key="IND" onClick={handleClick}>IND</Menu.Item>
+      <Menu.Item key="CHI" onClick={handleClick}>CHI</Menu.Item>
+      <Menu.Item key="DET" onClick={handleClick}>DET</Menu.Item>
+      <Menu.Item key="CLE" onClick={handleClick}>CLE</Menu.Item>
+    </SubMenu>
+    <SubMenu key="sub3" title="Southeast">
+      <Menu.Item key="MIA" onClick={handleClick}>MIA</Menu.Item>
+      <Menu.Item key="ORL" onClick={handleClick}>ORL</Menu.Item>
+      <Menu.Item key="CHA" onClick={handleClick}>CHA</Menu.Item>
+      <Menu.Item key="WAS" onClick={handleClick}>WAS</Menu.Item>
+      <Menu.Item key="ATL" onClick={handleClick}>ATL</Menu.Item>
+    </SubMenu>
+    <SubMenu key="sub4" title="Northwest">
+      <Menu.Item key="DEN" onClick={handleClick}>DEN</Menu.Item>
+      <Menu.Item key="OKC" onClick={handleClick}>OKC</Menu.Item>
+      <Menu.Item key="UTA" onClick={handleClick}>UTA</Menu.Item>
+      <Menu.Item key="POR" onClick={handleClick}>POR</Menu.Item>
+      <Menu.Item key="MIN" onClick={handleClick}>MIN</Menu.Item>
+    </SubMenu>
+    <SubMenu key="sub5" title="Pacific">
+      <Menu.Item key="LAL" onClick={handleClick}>LAL</Menu.Item>
+      <Menu.Item key="LAC" onClick={handleClick}>LAC</Menu.Item>
+      <Menu.Item key="PHX" onClick={handleClick}>PHX</Menu.Item>
+      <Menu.Item key="SAC" onClick={handleClick}>SAC</Menu.Item>
+      <Menu.Item key="GSW" onClick={handleClick}>GSW</Menu.Item>
+    </SubMenu>
+    <SubMenu key="sub6" title="Southwest">
+      <Menu.Item key="HOU" onClick={handleClick}>HOU</Menu.Item>
+      <Menu.Item key="DAL" onClick={handleClick}>DAL</Menu.Item>
+      <Menu.Item key="MEM" onClick={handleClick}>MEM</Menu.Item>
+      <Menu.Item key="SAS" onClick={handleClick}>SAS</Menu.Item>
+      <Menu.Item key="NOP" onClick={handleClick}>NOP</Menu.Item>
+    </SubMenu>
+  </Menu>
+  );
+  return (
+    <React.Fragment> 
+  <Dropdown overlay={menu}>
+  <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+    {selectedTeam===null? "Select Team" : selectedTeam} <DownOutlined />
+  </a>
+  </Dropdown>
+  </React.Fragment>
   );
 }
-
-// <ListItem button onClick={handleDivClick} id="Atlantic">
-//         <ListItemText primary="Atlantic" />
-//         {openAtl ? <ExpandLess /> : <ExpandMore />}
-//       </ListItem>
-//       <Collapse in={openAtl} timeout="auto" unmountOnExit>
-//         <List component="div" disablePadding>
-//           <ListItem button className={classes.nested}>
-//             <ListItemText primary="Starred" />
-//           </ListItem>
-//         </List>
-//       </Collapse>
-//       <ListItem button onClick={handleDivClick} id="Central">
-//         <ListItemText primary="Central" />
-//         {openCen ? <ExpandLess /> : <ExpandMore />}
-//       </ListItem>
-//       <Collapse in={openCen} timeout="auto" unmountOnExit>
-//         <List component="div" disablePadding>
-//           <ListItem button className={classes.nested}>
-//             <ListItemText primary="Starred" />
-//           </ListItem>
-//         </List>
-//       </Collapse>
-//       <ListItem button onClick={handleDivClick} id="Southeast">
-//         <ListItemText primary="Southeast" />
-//         {openSE ? <ExpandLess /> : <ExpandMore />}
-//       </ListItem>
-//       <Collapse in={openSE} timeout="auto" unmountOnExit>
-//         <List component="div" disablePadding>
-//           <ListItem button className={classes.nested}>
-//             <ListItemText primary="Starred" />
-//           </ListItem>
-//         </List>
-//       </Collapse>
-//       <ListItem button onClick={handleDivClick} id="Northwest">
-//         <ListItemText primary="Northwest" />
-//         {openNW ? <ExpandLess /> : <ExpandMore />}
-//       </ListItem>
-//       <Collapse in={openNW} timeout="auto" unmountOnExit>
-//         <List component="div" disablePadding>
-//           <ListItem button className={classes.nested}>
-//             <ListItemText primary="Starred" />
-//           </ListItem>
-//         </List>
-//       </Collapse><ListItem button onClick={handleDivClick} id="Pacific">
-//         <ListItemText primary="Pacific" />
-//         {openPac ? <ExpandLess /> : <ExpandMore />}
-//       </ListItem>
-//       <Collapse in={openPac} timeout="auto" unmountOnExit>
-//         <List component="div" disablePadding>
-//           <ListItem button className={classes.nested}>
-//             <ListItemText primary="Starred" />
-//           </ListItem>
-//         </List>
-//       </Collapse><ListItem button onClick={handleDivClick} id="Southwest">
-//         <ListItemText primary="Southwest" />
-//         {openSW ? <ExpandLess /> : <ExpandMore />}
-//       </ListItem>
-//       <Collapse in={openSW} timeout="auto" unmountOnExit>
-//         <List component="div" disablePadding>
-//           <ListItem button className={classes.nested}>
-//             <ListItemText primary="Starred" />
-//           </ListItem>
-//         </List>
-//       </Collapse>
